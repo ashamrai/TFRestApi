d9b471c4b50d133c5c9047c7d0fe54ef5fea0e7e
ashamrai <ashamrai@outlook.com>
2018-12-27 20:34:58 +0200

    manage areas ans iterations in teams

--- 10.TFRestApiAppManageTeamSettings/TFRestApiApp/Program.cs
+++ 10.TFRestApiAppManageTeamSettings/TFRestApiApp/Program.cs
@@ -45,10 +45,12 @@ namespace TFRestApiApp
                 string teamProject = ""; // team project for tests
                 string team = teamProject + " Team"; // team to tests (default team)                
 
-                //ConnectWithDefaultCreds(TFUrl);
-                ConnectWithPAT(TFUrl, UserPAT);
+                ConnectWithDefaultCreds(TFUrl);
+                //ConnectWithPAT(TFUrl, UserPAT);
 
-                //AddTeamIterations(teamProject, team);
+                AddTeamIterations(teamProject, team);
+                DeleteTeamIteration(teamProject, team);
+                AddTeamAreas(teamProject, team);
                 GetTeamSettings(teamProject, team);
                 GetTeamAreas(teamProject, team);
                 GetTeamIterations(teamProject, team);
@@ -61,6 +63,41 @@ namespace TFRestApiApp
             }
         }
 
+        /// <summary>
+        /// Add existing iteraions to a team
+        /// </summary>
+        /// <param name="TeamProjectName"></param>
+        /// <param name="TeamName"></param>
+        static void AddTeamAreas(string TeamProjectName, string TeamName)
+        {
+            TeamContext teamContext = new TeamContext(TeamProjectName, TeamName);
+
+            Console.WriteLine("Add areas to the team " + TeamName);
+
+            string[] areas = { @"Application\WinClient", @"Application\WebClient" };
+
+            TeamFieldValues currentTeamAreas = WorkClient.GetTeamFieldValuesAsync(teamContext).Result; // get current areas
+
+            TeamFieldValuesPatch teamAreasPatch = new TeamFieldValuesPatch();
+            
+            List<TeamFieldValue> newTeamAreas = new List<TeamFieldValue>(currentTeamAreas.Values); // just copy old areas. Here we may remove unneeded areas
+
+            foreach (string area in areas)
+                newTeamAreas.Add(new TeamFieldValue { Value = TeamProjectName + "\\" + area, IncludeChildren = false }); // add new areas
+
+            teamAreasPatch.DefaultValue = currentTeamAreas.DefaultValue;
+            teamAreasPatch.Values = newTeamAreas;            
+
+            TeamFieldValues updatedTeamAreas = WorkClient.UpdateTeamFieldValuesAsync(teamAreasPatch, teamContext).Result;
+
+            Console.WriteLine("Areas has been added");
+        }
+
+        /// <summary>
+        /// Add existing areas
+        /// </summary>
+        /// <param name="TeamProjectName"></param>
+        /// <param name="TeamName"></param>
         static void AddTeamIterations(string TeamProjectName, string TeamName)
         {
             TeamContext teamContext = new TeamContext(TeamProjectName, TeamName);
@@ -71,13 +108,36 @@ namespace TFRestApiApp
 
             foreach (string it in iterations)
             {
-                WorkItemClassificationNode tfIt = WitClient.GetClassificationNodeAsync(TeamProjectName, TreeStructureGroup.Iterations, it).Result;
-                TeamSettingsIteration teamIt = WorkClient.PostTeamIterationAsync(new TeamSettingsIteration { Id = tfIt.Identifier }, teamContext).Result;
+                WorkItemClassificationNode projectIt = WitClient.GetClassificationNodeAsync(TeamProjectName, TreeStructureGroup.Iterations, it).Result; // get iteration from project
+                TeamSettingsIteration teamIt = WorkClient.PostTeamIterationAsync(new TeamSettingsIteration { Id = projectIt.Identifier }, teamContext).Result; //add iteration to a team by guid 
                 Console.WriteLine("Added iteration " + teamIt.Name);
             }
 
         }
 
+        /// <summary>
+        /// Remove iteration from team settings
+        /// </summary>
+        /// <param name="TeamProjectName"></param>
+        /// <param name="TeamName"></param>
+        static void DeleteTeamIteration(string TeamProjectName, string TeamName)
+        {
+            TeamContext teamContext = new TeamContext(TeamProjectName, TeamName);
+
+            Console.WriteLine("Remove iterations for the team " + TeamName);
+
+            string iterationToRemove = @"R2\R2.1\Ver2";
+
+            WorkItemClassificationNode projectIt = WitClient.GetClassificationNodeAsync(TeamProjectName, TreeStructureGroup.Iterations, iterationToRemove).Result;
+            WorkClient.DeleteTeamIterationAsync(teamContext, projectIt.Identifier).SyncResult(); //delete iteration from team settings by guid
+            Console.WriteLine("Removed iteration " + projectIt.Name);
+        }
+
+        /// <summary>
+        /// Get general team settings
+        /// </summary>
+        /// <param name="TeamProjectName"></param>
+        /// <param name="TeamName"></param>
         static void GetTeamSettings(string TeamProjectName, string TeamName)
         {
             TeamContext teamContext = new TeamContext(TeamProjectName, TeamName);
@@ -95,32 +155,42 @@ namespace TFRestApiApp
             foreach (var wday in teamSetting.WorkingDays) Console.WriteLine("\t" + wday.ToString());             
         }
 
+        /// <summary>
+        /// Get team ares
+        /// </summary>
+        /// <param name="TeamProjectName"></param>
+        /// <param name="TeamName"></param>
         static void GetTeamAreas(string TeamProjectName, string TeamName)
         {
             TeamContext teamContext = new TeamContext(TeamProjectName, TeamName);
 
-            TeamFieldValues teamFieldValues = WorkClient.GetTeamFieldValuesAsync(teamContext).Result;
+            TeamFieldValues teamAreas = WorkClient.GetTeamFieldValuesAsync(teamContext).Result; //Get All Areas
 
             Console.WriteLine("Areas of the team " + TeamName);
-            Console.WriteLine("Default Area: " + teamFieldValues.DefaultValue);
+            Console.WriteLine("Default Area: " + teamAreas.DefaultValue);
 
             Console.WriteLine("Team Areas  : ");
-            foreach (TeamFieldValue teamField in teamFieldValues.Values)
-                Console.WriteLine("\t" + teamField.Value + ((teamField.IncludeChildren)? " (include subareas)" : ""));
+            foreach (TeamFieldValue teamField in teamAreas.Values)
+                Console.WriteLine("\t" + teamField.Value + ((teamField.IncludeChildren)? " (include sub-areas)" : ""));
         }
 
+        /// <summary>
+        /// Get Team Iterations
+        /// </summary>
+        /// <param name="TeamProjectName"></param>
+        /// <param name="TeamName"></param>
         static void GetTeamIterations(string TeamProjectName, string TeamName)
         {
             TeamContext teamContext = new TeamContext(TeamProjectName, TeamName);
 
             Console.WriteLine("Iterations of the team " + TeamName);
 
-            TeamSettingsIteration currentiteration = (WorkClient.GetTeamIterationsAsync(teamContext, "Current").Result).FirstOrDefault(); // get current iteration
+            TeamSettingsIteration currentiteration = (WorkClient.GetTeamIterationsAsync(teamContext, "Current").Result).FirstOrDefault(); // get a current iteration
 
             if (currentiteration != null)
                 Console.WriteLine("Current iteration - {0} : {1}-{2}", currentiteration.Name, currentiteration.Attributes.StartDate, currentiteration.Attributes.FinishDate);
 
-            List<TeamSettingsIteration> teamIterations = WorkClient.GetTeamIterationsAsync(teamContext).Result;   
+            List<TeamSettingsIteration> teamIterations = WorkClient.GetTeamIterationsAsync(teamContext).Result;   //get all iterations
 
             Console.WriteLine("Team Iterations: ");
             foreach (TeamSettingsIteration teamIteration in teamIterations)
