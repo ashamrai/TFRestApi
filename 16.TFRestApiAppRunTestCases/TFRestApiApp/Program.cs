2951f38174ad419b9620f167243b1b9715655ca1
ashamrai <ashamrai@outlook.com>
2019-03-29 17:02:11 +0200

    add test runs for test plan

---
+++ 16.TFRestApiAppRunTestCases/TFRestApiApp/Program.cs
@@ -0,0 +1,261 @@
+﻿using Microsoft.TeamFoundation.Build.WebApi;
+using Microsoft.TeamFoundation.Core.WebApi;
+using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.TeamFoundation.TestManagement.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using Microsoft.VisualStudio.Services.WebApi.Patch;
+using Microsoft.VisualStudio.Services.WebApi.Patch.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFRestApiApp
+{     
+
+
+    class Program
+    {
+        //static readonly string TFUrl = "http://tfs-srv:8080/tfs/DefaultCollection/"; //for tfs
+        static readonly string TFUrl = "https://dev.azure.com/<org>/"; // for devops azure 
+        static readonly string UserAccount = "";
+        static readonly string UserPassword = "";
+        static readonly string UserPAT = "<pat>"; //https://docs.microsoft.com/en-us/azure/devops/organizations/accounts/use-personal-access-tokens-to-authenticate?view=azure-devops
+
+        static WorkItemTrackingHttpClient WitClient;
+        static BuildHttpClient BuildClient;
+        static ProjectHttpClient ProjectClient;
+        static GitHttpClient GitClient;
+        static TfvcHttpClient TfvsClient;
+        static TestManagementHttpClient TestManagementClient;
+
+        static void Main(string[] args)
+        {
+            
+
+            try
+            {
+                ConnectWithPAT(TFUrl, UserPAT);
+
+                string teamProjectName = "<Team Project Name>";
+                int testPlanId = 1177;  // Id of test plan with test cases             
+                string staticSuiteName = "Static Suite"; //path to static suite with test cases
+                int[] testCaseIds = { 1196, 1214 }; // existing ids of test cases
+
+
+                CreateTestResultCompleted(teamProjectName, testPlanId, staticSuiteName, testCaseIds);
+                CreateTestResultFailed(teamProjectName, testPlanId, staticSuiteName, testCaseIds);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+                if (ex.InnerException != null) Console.WriteLine("Detailed Info: " + ex.InnerException.Message);
+                Console.WriteLine("Stack:\n" + ex.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// Create successed test run
+        /// </summary>
+        /// <param name="TeamProjectName"></param>
+        /// <param name="TestPlanId"></param>
+        /// <param name="StaticSuitePath"></param>
+        /// <param name="TestCaseIds"></param>
+        private static void CreateTestResultCompleted(string TeamProjectName, int TestPlanId, string StaticSuitePath, int[] TestCaseIds)
+        {
+            TestPlan testPlan = TestManagementClient.GetPlanByIdAsync(TeamProjectName, TestPlanId).Result;
+
+            var testPlanRef = new Microsoft.TeamFoundation.TestManagement.WebApi.ShallowReference(testPlan.Id.ToString(), testPlan.Name, testPlan.Url);
+
+            RunCreateModel runCreate = new RunCreateModel(
+                name: "Test run from console - completed",
+                plan: testPlanRef,
+                startedDate: DateTime.Now.ToString("o"),                
+                isAutomated: true                
+                );
+
+            TestRun testRun = TestManagementClient.CreateTestRunAsync(runCreate, TeamProjectName).Result;
+
+            List<TestCaseResult> testResults = new List<TestCaseResult>();
+
+            foreach (int testCaseId in TestCaseIds) testResults.Add(PassedTest(TeamProjectName, TestPlanId, StaticSuitePath, testCaseId));
+
+            testResults = TestManagementClient.AddTestResultsToTestRunAsync(testResults.ToArray(), TeamProjectName, testRun.Id).Result;
+
+            RunUpdateModel runUpdateModel = new RunUpdateModel(
+                completedDate: DateTime.Now.ToString("o"),
+                state: Enum.GetName(typeof(TestRunState), TestRunState.Completed)                
+                );
+
+            testRun = TestManagementClient.UpdateTestRunAsync(runUpdateModel, TeamProjectName, testRun.Id).Result;
+
+            PrintBasicRunInfo(testRun);
+        }
+
+        /// <summary>
+        /// Create failed test run
+        /// </summary>
+        /// <param name="TeamProjectName"></param>
+        /// <param name="TestPlanId"></param>
+        /// <param name="StaticSuitePath"></param>
+        /// <param name="TestCaseIds"></param>
+        private static void CreateTestResultFailed(string TeamProjectName, int TestPlanId, string StaticSuitePath, int[] TestCaseIds)
+        {
+            TestPlan testPlan = TestManagementClient.GetPlanByIdAsync(TeamProjectName, TestPlanId).Result;
+
+            var testPlanRef = new Microsoft.TeamFoundation.TestManagement.WebApi.ShallowReference(testPlan.Id.ToString(), testPlan.Name, testPlan.Url);
+
+            RunCreateModel runCreate = new RunCreateModel(
+                name: "Test run from console - failed",
+                plan: testPlanRef,
+                startedDate: DateTime.Now.ToString("o"),
+                isAutomated: true
+                );
+
+            TestRun testRun = TestManagementClient.CreateTestRunAsync(runCreate, TeamProjectName).Result;
+
+            List<TestCaseResult> testResults = new List<TestCaseResult>();
+
+            foreach (int testCaseId in TestCaseIds) testResults.Add(FailedTest(TeamProjectName, TestPlanId, StaticSuitePath, testCaseId, testRun.Id));
+
+            testResults = TestManagementClient.AddTestResultsToTestRunAsync(testResults.ToArray(), TeamProjectName, testRun.Id).Result;
+
+            var definedTestResults = TestManagementClient.GetTestResultsAsync(TeamProjectName, testRun.Id).Result;
+            
+            RunUpdateModel runUpdateModel = new RunUpdateModel(
+                errorMessage: "Test failed",
+                completedDate: DateTime.Now.ToString("o"),
+                state: Enum.GetName(typeof(TestRunState), TestRunState.NeedsInvestigation)
+                );
+
+            testRun = TestManagementClient.UpdateTestRunAsync(runUpdateModel, TeamProjectName, testRun.Id).Result;
+
+            PrintBasicRunInfo(testRun);
+        }
+
+        /// <summary>
+        /// Create struct for passed test
+        /// </summary>
+        /// <param name="TeamProjectName"></param>
+        /// <param name="TestPlanId"></param>
+        /// <param name="StaticSuitePath"></param>
+        /// <param name="TestCaseId"></param>
+        /// <returns></returns>
+        static TestCaseResult PassedTest(string TeamProjectName, int TestPlanId, string StaticSuitePath, int TestCaseId)
+        {
+            int testSuiteId = GetSuiteId(TeamProjectName, TestPlanId, StaticSuitePath);
+            TestPoint testPoint = TestManagementClient.GetPointsAsync(TeamProjectName, TestPlanId, testSuiteId, testCaseId: TestCaseId.ToString()).Result.FirstOrDefault();
+
+            TestCaseResult testCaseResult = new TestCaseResult();
+            testCaseResult.Outcome = Enum.GetName(typeof(TestOutcome), TestOutcome.Passed);
+            testCaseResult.TestPoint = new Microsoft.TeamFoundation.TestManagement.WebApi.ShallowReference(testPoint.Id.ToString(), url: testPoint.Url);
+            testCaseResult.CompletedDate = DateTime.Now;            
+            testCaseResult.State = Enum.GetName(typeof(TestRunState), TestRunState.Completed);            
+
+            return testCaseResult;
+        }
+
+        /// <summary>
+        /// Create sruct for failed test
+        /// </summary>
+        /// <param name="TeamProjectName"></param>
+        /// <param name="TestPlanId"></param>
+        /// <param name="StaticSuitePath"></param>
+        /// <param name="TestCaseId"></param>
+        /// <param name="TestRunId"></param>
+        /// <returns></returns>
+        static TestCaseResult FailedTest(string TeamProjectName, int TestPlanId, string StaticSuitePath, int TestCaseId, int TestRunId)
+        {
+            int testSuiteId = GetSuiteId(TeamProjectName, TestPlanId, StaticSuitePath);
+            TestPoint testPoint = TestManagementClient.GetPointsAsync(TeamProjectName, TestPlanId, testSuiteId, testCaseId: TestCaseId.ToString()).Result.FirstOrDefault();
+
+            TestCaseResult testCaseResult = new TestCaseResult();
+            testCaseResult.Outcome = Enum.GetName(typeof(TestOutcome), TestOutcome.Failed);
+            testCaseResult.TestPoint = new Microsoft.TeamFoundation.TestManagement.WebApi.ShallowReference(testPoint.Id.ToString(), url: testPoint.Url);
+            testCaseResult.CompletedDate = DateTime.Now;
+            testCaseResult.ErrorMessage = "Test Case " + TestCaseId + " failed";
+            testCaseResult.State = Enum.GetName(typeof(TestRunState), TestRunState.Completed);
+            testCaseResult.StackTrace = "Add StackTrace here";
+         
+            return testCaseResult;
+        }
+       
+        /// <summary>
+        /// Get ID on an existing test suite by path in test plan
+        /// </summary>
+        /// <param name="TeamProjectName"></param>
+        /// <param name="TestPlanId"></param>
+        /// <param name="SuitePath"></param>
+        /// <returns></returns>
+        static int GetSuiteId(string TeamProjectName, int TestPlanId, string SuitePath)
+        {
+            TestPlan testPlan = TestManagementClient.GetPlanByIdAsync(TeamProjectName, TestPlanId).Result;                       
+
+            int parentSuiteId = 0;
+
+            if (int.TryParse(testPlan.RootSuite.Id, out parentSuiteId))
+            {
+                if (SuitePath == "") return parentSuiteId;
+
+                string[] pathArray = SuitePath.Split(new char[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < pathArray.Length; i++)
+                {
+                    TestSuite testSuite = TestManagementClient.GetTestSuiteByIdAsync(TeamProjectName, TestPlanId, parentSuiteId, 1).Result;
+
+                    string parentSuiteIdStr = (from ts in testSuite.Suites where ts.Name == pathArray[i] select ts.Id).FirstOrDefault();
+
+                    if (!int.TryParse(parentSuiteIdStr, out parentSuiteId)) break;
+
+                    if (i == pathArray.Length - 1) return parentSuiteId;
+                }                
+            }
+
+            return 0;
+        }
+
+        static void PrintBasicRunInfo(TestRun testRun)
+        {
+            Console.WriteLine("Information for test run:" + testRun.Id);
+            Console.WriteLine("Automated - {0}; Start Date - '{1}'; Completed date - '{2}'", (testRun.IsAutomated) ? "Yes" : "No", testRun.StartedDate.ToString(), testRun.CompletedDate.ToString());
+            Console.WriteLine("Total tests - {0}; Passed tests - {1}", testRun.TotalTests, testRun.PassedTests);
+        }
+
+        #region create new connections
+        static void InitClients(VssConnection Connection)
+        {
+            WitClient = Connection.GetClient<WorkItemTrackingHttpClient>();
+            BuildClient = Connection.GetClient<BuildHttpClient>();
+            ProjectClient = Connection.GetClient<ProjectHttpClient>();
+            GitClient = Connection.GetClient<GitHttpClient>();
+            TfvsClient = Connection.GetClient<TfvcHttpClient>();
+            TestManagementClient = Connection.GetClient<TestManagementHttpClient>();
+        }
+
+        static void ConnectWithDefaultCreds(string ServiceURL)
+        {
+            VssConnection connection = new VssConnection(new Uri(ServiceURL), new VssCredentials());
+            InitClients(connection);
+        }
+
+        static void ConnectWithCustomCreds(string ServiceURL, string User, string Password)
+        {
+            VssConnection connection = new VssConnection(new Uri(ServiceURL), new WindowsCredential(new NetworkCredential(User, Password)));
+            InitClients(connection);
+        }
+
+        static void ConnectWithPAT(string ServiceURL, string PAT)
+        {
+            VssConnection connection = new VssConnection(new Uri(ServiceURL), new VssBasicCredential(string.Empty, PAT));
+            InitClients(connection);
+        }
+        #endregion
+    }
+}
