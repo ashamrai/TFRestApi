034ed3b4a009c0097d4951cb423c7171669a1b14
Aleksandr Shamrai <ashamrai@outlook.com>
2020-02-20 12:31:42 +0200

    add parent link

--- 07.TFRestApiAppWorkItemLinks/TFRestApiApp/Program.cs
+++ 07.TFRestApiAppWorkItemLinks/TFRestApiApp/Program.cs
@@ -21,12 +21,12 @@ namespace TFRestApiApp
 {
     class Program
     {
-        static readonly string TFUrl = "http://tfs-srv:8080/tfs/DefaultCollection/"; //for tfs
-        //static readonly string TFUrl = "https://dev.azure.com/<your_org>/"; // for devops azure 
+        //static readonly string TFUrl = "http://tfs-srv:8080/tfs/DefaultCollection/"; //for tfs
+        static readonly string TFUrl = "https://dev.azure.com/<org>/"; // for devops azure 
         static readonly string UserAccount = "";
         static readonly string UserPassword = "";
-        static readonly string UserPAT = "<your_pat>";
-        
+        static readonly string UserPAT = "<your_pat>"; //https://docs.microsoft.com/en-us/azure/devops/organizations/accounts/use-personal-access-tokens-to-authenticate
+
 
         static WorkItemTrackingHttpClient WitClient;
         static BuildHttpClient BuildClient;
@@ -38,13 +38,16 @@ namespace TFRestApiApp
         static void Main(string[] args)
         {
             string teamProject = ""; // team project for new work item
-            int sourceId = -1; // existing work item to duplicatae
+            int sourceId = -1; // existing work item to duplicate
+            int parentId = -1; // existing work item to add as parent
             
-            ConnectWithDefaultCreds(TFUrl);
+            ConnectWithPAT(TFUrl, UserPAT);
 
             var newWi = DuplicateWorkItem(teamProject, sourceId); // create a duplicate and link it
             RemoveWorkItemLink(sourceId, newWi.Id.Value, RelConstants.DuplicateRefStr); // remove the duplicate
             UpdateWorkItemLink(sourceId, newWi.Id.Value, RelConstants.RelatedRefStr); // update comments and unlock links
+
+            if (parentId > 0) AddParentLink(newWi.Id.Value, parentId);
         }   
         
 
@@ -77,11 +80,11 @@ namespace TFRestApiApp
                         fields.Add(RelConstants.LinkKeyForDict + lnk.Rel + lnk.Url, lnk);
 
             //add duplicate link to source work item
-            fields.Add(RelConstants.LinkKeyForDict + RelConstants.DuplicateOfRefStr + DuplicateId,
+            fields.Add(RelConstants.LinkKeyForDict + RelConstants.DuplicateOfRefStr + DuplicateId, // to use as unique key
                 CreateNewLinkObject(RelConstants.DuplicateOfRefStr, wi.Url, "Duplicate " + wi.Id));
 
             //add related link and lock it
-            fields.Add(RelConstants.LinkKeyForDict + RelConstants.RelatedRefStr + DuplicateId,
+            fields.Add(RelConstants.LinkKeyForDict + RelConstants.RelatedRefStr + DuplicateId, // to use as unique key
                 CreateNewLinkObject(RelConstants.RelatedRefStr, wi.Url, IsLocked: true));
 
             var dupWi = SubmitWorkItem(fields, 0, TeamProjectName, wi.Fields["System.WorkItemType"].ToString());
@@ -199,6 +202,39 @@ namespace TFRestApiApp
         }
 
         /// <summary>
+        /// Add new parent link to existing work item
+        /// </summary>
+        /// <param name="WiId"></param>
+        /// <param name="ParentWiId"></param>
+        /// <returns></returns>
+        static WorkItem AddParentLink(int WiId, int ParentWiId)
+        {
+            WorkItem wi = WitClient.GetWorkItemAsync(WiId, expand: WorkItemExpand.Relations).Result;
+            bool parentExists = false;
+
+            // check existing parent link
+            if (wi.Relations != null)
+                if (wi.Relations.Where(x => x.Rel == RelConstants.ParentRefStr).FirstOrDefault() != null) 
+                    parentExists = true;
+
+            if (!parentExists)
+            {
+                WorkItem parentWi = WitClient.GetWorkItemAsync(ParentWiId).Result; // get parent to retrieve its url
+
+                Dictionary<string, object> fields = new Dictionary<string, object>();
+
+                fields.Add(RelConstants.LinkKeyForDict + RelConstants.ParentRefStr + parentWi.Id, // to use as unique key
+                CreateNewLinkObject(RelConstants.ParentRefStr, parentWi.Url, "Parent " + parentWi.Id));
+
+                return SubmitWorkItem(fields, WiId);
+            }
+
+            Console.WriteLine("Work Item " + WiId + " contains a parent link");
+
+            return null;
+        }
+
+        /// <summary>
         /// Create or update a work item
         /// </summary>
         /// <param name="WIId"></param>
@@ -257,7 +293,7 @@ namespace TFRestApiApp
 
             public const string RelatedRefStr = "System.LinkTypes.Related";
             public const string ChildRefStr = "System.LinkTypes.Hierarchy-Forward";
-            public const string ParrentRefStr = "System.LinkTypes.Hierarchy-Reverse";            
+            public const string ParentRefStr = "System.LinkTypes.Hierarchy-Reverse";            
             public const string DuplicateRefStr = "System.LinkTypes.Duplicate-Forward";
             public const string DuplicateOfRefStr = "System.LinkTypes.Duplicate-Reverse";
             public const string SuccessorRefStr = "System.LinkTypes.Dependency-Forward";
@@ -266,8 +302,8 @@ namespace TFRestApiApp
             public const string TestsRefStr = "Microsoft.VSTS.Common.TestedBy-Reverse";
             public const string TestCaseRefStr = "Microsoft.VSTS.TestCase.SharedStepReferencedBy-Forward";
             public const string SharedStepsRefStr = "Microsoft.VSTS.TestCase.SharedStepReferencedBy-Reverse";
-            public const string AffectsRefStr = "Microsoft.VSTS.Common.Affects.Forward";
-            public const string AffectedByRefStr = "Microsoft.VSTS.Common.Affects.Reverse";            
+            public const string AffectsRefStr = "Microsoft.VSTS.Common.Affects-Forward";
+            public const string AffectedByRefStr = "Microsoft.VSTS.Common.Affects-Reverse";            
             public const string AttachmentRefStr = "AttachedFile";
             public const string HyperLinkRefStr = "Hyperlink";
             public const string ArtifactLinkRefStr = "ArtifactLink";
